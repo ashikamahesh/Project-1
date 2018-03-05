5c84feeb7f7167f60bbed04799ca23c971947cbb
ashika <ashikamahesh.97@gmail.com>
2018-03-05 11:16:41 +0530

    This is my first commit

---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+int i,j;
+printf ("Enter the numbers\n");
+printf("Enter first number\n");
+scanf("%d",&i);
+printf("Enter second number\n");
+scanf("%d",&j);
+printf("Sum of numbers is %d",i+j);
+}
